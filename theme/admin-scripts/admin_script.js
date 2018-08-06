2187e2ba969e1f1cdb9fa63efda5930f1ddcb48d
Philipp Mainz <pmainz@varion.de>
2018-08-06 15:19:03 +0200

    resolved issue due to mal merge handling

--- theme/admin-scripts/admin_script.js
+++ theme/admin-scripts/admin_script.js
@@ -3,12 +3,7 @@
 	$(document).ready(function(){
 
 		$('body').on('click', 'button.media-upload-btn', function (event){
-<<<<<<< HEAD
-			console.log(event);
-=======
 			var etID = event.target.id;
-			console.log(etID);
->>>>>>> 3f0e0e0961ec2c2be4617460bb750e315d08c2da
 			event.preventDefault();
 			var imageUploader =  new wp.media.view.MediaFrame.Select({
 				'title':'Select Media',
@@ -18,12 +13,8 @@
 
 			imageUploader.on("select",function(){
 				var image = imageUploader.state().get('selection').toJSON();
-<<<<<<< HEAD
-				$('#widget-wp_wt_text00001-3-media+input').val('asldfjk')
-=======
 				$('#'+etID+'+input').val(image[0].url).trigger('change');
 				$('#display-'+etID).css('background-image','url("'+image[0].url+'")');
->>>>>>> 3f0e0e0961ec2c2be4617460bb750e315d08c2da
 			});
 		});
 	});
